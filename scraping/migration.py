8420a0440b6a5a471bb643ae9be1f52a6691eef0
Techie <149437995+FarRza111@users.noreply.github.com>
2023-12-11 00:53:55 -0800

    Create migration.py

---
+++ scraping/migration.py
@@ -0,0 +1,47 @@
+import sqlite3
+import pandas as pd
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Exception as e:
+        print(e)
+
+    return conn
+
+conn = create_connection('bina_az.db')
+
+def create_project(conn, project):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param project:
+    :return: project id
+    """
+    sql = ''' INSERT INTO bina(city,date, price, currency, room_size, location)
+              VALUES(?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+    return cur.lastrowid
+
+
+def main():
+
+    df = pd.read_excel('mdata.xlsx')
+    mylist = list(map(tuple, df.to_numpy()))
+    for i in mylist:
+        create_project(conn, i)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
